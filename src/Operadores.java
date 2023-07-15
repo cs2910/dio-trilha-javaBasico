23217db4701d8c357e98341e805c0505da49a085
claudio.schwambach <cs2910@gmail.com>
2023-07-15 19:46:51 -0300

    Primeiro commit

---
+++ src/Operadores.java
@@ -0,0 +1,17 @@
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+public class Operadores {
+    public static void main(String[] args) {
+        String nome = "Claudio"+" Schwambach";
+        System.out.println(nome);
+        System.out.println(1+1+"1");
+        System.out.println(21+"1"+21);
+        System.out.println("3"+1+1+3);
+        int idade = 18;
+        idade ++;
+        System.out.println(idade);
+        idade *= 3;
+        System.out.println(idade);
+    }
+
+}
--- src/TiposVariaveis.java
+++ src/TiposVariaveis.java
@@ -2,6 +2,12 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class TiposVariaveis {
     public static void main(String[] args) {
-        System.out.print("Ola Claudio Schwambach");
+       // System.out.print("Ola Claudio Schwambach");
+       double salario = 2500.00;
+       final double PI = 3.14;
+       int peso = 90;
+       System.out.print(PI);
+
+
     }
 }
